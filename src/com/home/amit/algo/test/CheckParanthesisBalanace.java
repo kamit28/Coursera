00a0c86f7f3c0406466761452aa70f5a1f2a3127
kamit28 <kamit28@gmail.com>
2017-05-18 13:57:47 +1000

    Change accepted

--- src/com/home/amit/algo/test/CheckParanthesisBalanace.java
+++ src/com/home/amit/algo/test/CheckParanthesisBalanace.java
@@ -3,47 +3,46 @@
  */
 package com.home.amit.algo.test;
 
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
  * @author Amit
  *
  */
-
 public class CheckParanthesisBalanace {
 
-	public static void main(String[] argh) {
-		Scanner sc = new Scanner(System.in);
-		Stack<Character> parenStack = new Stack<Character>();
+	public static void main(String[] argh) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// Stack<Character> parenStack = new Stack<Character>();
+		Deque<Character> parenStack = new ArrayDeque<Character>();
 		char val;
 		char val1;
-		while (sc.hasNext()) {
-			String input = sc.next();
-			for (int i = 0; i < input.length(); i++) {
-				val = input.charAt(i);
-				parenStack.push(val);
-			}
-			Stack<Character> tempStack = new Stack<Character>();
-			while (!parenStack.isEmpty()) {
-				val = parenStack.pop();
-				if (!parenStack.isEmpty()) {
-					val1 = parenStack.peek();
-					if ((val == ')' && val1 == '(')
-							|| (val == '}' && val1 == '{')
-							|| (val == ']' && val1 == '[')) {
-						parenStack.pop();
-						while (!tempStack.isEmpty()) {
-							parenStack.push(tempStack.pop());
-						}
-					} else
-						tempStack.push(val);
-				} else {
-					tempStack.push(val);
-				}
-			}
-			System.out.println(tempStack.isEmpty());
+		String input = br.readLine();
+		for (int i = 0; i < input.length(); i++) {
+			parenStack.push(input.charAt(i));
 		}
-		sc.close();
+		// Stack<Character> tempStack = new Stack<Character>();
+		Deque<Character> tempStack = new ArrayDeque<Character>();
+		while (!parenStack.isEmpty()) {
+			val = parenStack.pop();
+			if (!parenStack.isEmpty()) {
+				val1 = parenStack.peek();
+				if ((val == ')' && val1 == '(') || (val == '}' && val1 == '{')
+						|| (val == ']' && val1 == '[')) {
+					parenStack.pop();
+					while (!tempStack.isEmpty()) {
+						parenStack.push(tempStack.pop());
+					}
+				} else
+					tempStack.push(val);
+			} else {
+				tempStack.push(val);
+			}
+		}
+		System.out.println(tempStack.isEmpty() ? "True" : "False");
 	}
 }
