843713b0d248f292d941d8b6385b265abe0eaa47
kamit28 <kamit28@gmail.com>
2017-05-18 13:58:19 +1000

    Change accepted

--- src/com/home/amit/algo/test/MinInRotatedArrayTest.java
+++ src/com/home/amit/algo/test/MinInRotatedArrayTest.java
@@ -15,7 +15,7 @@ public class MinInRotatedArrayTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] arr = { 14, 2, 3, 4, 5, 8, 10, 11 };
+		int[] arr = { 11, 14, 2, 3, 4, 5, 8, 10 };
 
 		System.out.println(MinInRotatedArray.findMin(arr));
 		System.out.println(MinInRotatedArray.findMin_binarySearch(arr));
